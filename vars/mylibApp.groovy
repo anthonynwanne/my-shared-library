dda0df66eb395fa6599524ff0a276e4cc567b362
anthonynwanne <anthony.nwabunwanne@live.com>
2022-12-08 08:21:11 +0100

    mylibrary-added
    Signed-off-by: anthonynwanne <anthony.nwabunwanne@live.com>

---
+++ vars/mylibApp.groovy
@@ -0,0 +1,22 @@
+pipeline {
+       agent any
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'lscpu'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'df -g'
+               }
+           }
+       }
+}
